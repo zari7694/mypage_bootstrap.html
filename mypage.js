4a266b74a31d1dbda2476cf71094a3ffb899b5fb
zari7694 <49491356+zari7694@users.noreply.github.com>
2020-02-28 12:17:15 -0700

    working js with added text show/hide function

--- mypage.js
+++ mypage.js
@@ -6,16 +6,21 @@ function addCard(cardID){
   var tweetID = cardID + "Tweet";
   var count = $(parent).children().length;
   tweetID += (count - 3);
-  //console.log(count);
-
-  var tweet = $('<div class="cardTweet">  <button class="button buttonDelete" onclick=deleteCard()>-</button>  <img class="w3-round" src="https://www.wnmufm.org/sites/wnmu/files/styles/medium/public/201707/twitter_logo.png" alt="Twitter Logo" width=60;height=30;position=relative;left=45;>  <div class="w3-display-right w3-container">Tweet</div>  <div class="w3-display-bottommiddle">Sample tweet goes here!</div> </div>');
-  tweet.id = tweetID;
+  console.log(count);
+  var tweet = $('<div class="cardTweet" id="'+tweetID+'">  <button class="button buttonDelete" onclick=deleteCard(\''+tweetID+'\',\''+cardID+'\')>-</button>  <img class="w3-round" src="https://www.wnmufm.org/sites/wnmu/files/styles/medium/public/201707/twitter_logo.png" alt="Twitter Logo" width=60;height=30;position=relative;left=45;>  <div class="w3-display-right w3-container">Tweet</div>  <div class="w3-display-bottommiddle">Tweet ID:  '+tweetID+'</div> </div>');
 
   tweet.appendTo(parent);
-  //console.log(tweet.id);
 }//^
 
-function deleteCard(tweetID){
-  console.log(event.srcElement.offsetParent);
-  //document.getElementById(tweetID).remove();
+function deleteCard(tweetID, cardID){
+  //console.log(tweetID);
+  document.getElementById(tweetID).remove();
+
+  parent = "#" + cardID;
+  cardTextID = cardID + "Text";
+  count = $(parent).children().length;
+  console.log(count);
+  if(!(count - 4) )
+    document.getElementById(cardTextID).style.display = "block";
+
 }
